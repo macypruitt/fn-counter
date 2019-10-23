ce3f5cb1bfe75f3a6662aaaaf6e85d1a325359cc
Macy Pruitt <macypruitt@gmail.com>
2019-10-22 20:19:16 -0500

    added random number function, which now determines conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 import presentbox from './images/presentbox.png';
 import velo from './images/velo.png';
-import particlesJS from 'particles.js';
+
+////random number generating function makes magicNumber
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const magicNumber = getRandomInt(10);
 
 function App() {
 
+  ////state tracks count
   const [count, setCount] = useState(0);
 
- 
-
+  ////this box will change into velociraptor when count meets magicNumber
   let box = presentbox;
-
-  if(count>3){
+  if( count >= magicNumber ){
     box = velo;
   }
+
   return (
     <div className="App">
       <header className="App-header">
         
         <img src={box} className="present" alt="logo" />
+
         <p>
           I clicked {count} times
         </p>
+       
         <button onClick={() => setCount(count + 1)}>Clicky</button>
+
       </header>
     </div>
   );
